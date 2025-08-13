c9015a170780b6fc5f4076a0ee1ce3e23625ea6c
Abdur Rahim <abdur.rahim@pmtac.com>
2025-08-13 14:17:03 +0500

    Updated UI design

--- src/components/TechLogos.tsx
+++ src/components/TechLogos.tsx
@@ -3,58 +3,77 @@ import * as React from "react";
 // Lightweight logo grid using brand SVGs from Simple Icons CDN
 // This avoids bundling heavy icon libs and keeps assets cached on CDN
 
-const categories: { title: string; items: { name: string; src: string }[] }[] = [
-  {
-    title: "Programming Languages",
-    items: [
-      { name: "Java", src: "https://cdn.simpleicons.org/java" },
-      { name: "SQL", src: "https://cdn.simpleicons.org/sqlite" },
-    ],
-  },
-  {
-    title: "Frameworks & Libraries",
-    items: [
-      { name: "Spring", src: "https://cdn.simpleicons.org/spring" },
-      { name: "Spring Boot", src: "https://cdn.simpleicons.org/springboot" },
-      { name: "Hibernate", src: "https://cdn.simpleicons.org/hibernate" },
-      { name: "JUnit 5", src: "https://cdn.simpleicons.org/junit5" },
-    ],
-  },
-  {
-    title: "Tools & Platforms",
-    items: [
-      { name: "Docker", src: "https://cdn.simpleicons.org/docker" },
-      { name: "Git", src: "https://cdn.simpleicons.org/git" },
-      { name: "Maven", src: "https://cdn.simpleicons.org/apachemaven" },
-      { name: "RabbitMQ", src: "https://cdn.simpleicons.org/rabbitmq" },
-      { name: "Twilio", src: "https://cdn.simpleicons.org/twilio" },
-      { name: "Azure DevOps", src: "https://cdn.simpleicons.org/azuredevops" },
-      { name: "IntelliJ IDEA", src: "https://cdn.simpleicons.org/intellijidea" },
-      { name: "VS Code", src: "https://cdn.simpleicons.org/visualstudiocode" },
-    ],
-  },
-  {
-    title: "Databases",
-    items: [
-      { name: "MySQL", src: "https://cdn.simpleicons.org/mysql" },
-      { name: "PostgreSQL", src: "https://cdn.simpleicons.org/postgresql" },
-      { name: "SQL Server", src: "https://cdn.simpleicons.org/microsoftsqlserver" },
-    ],
-  },
-  {
-    title: "APIs & Testing",
-    items: [
-      { name: "Swagger", src: "https://cdn.simpleicons.org/swagger" },
-      { name: "Postman", src: "https://cdn.simpleicons.org/postman" },
-    ],
-  },
-  {
-    title: "Security",
-    items: [
-      { name: "JWT", src: "https://cdn.simpleicons.org/jsonwebtokens" },
-    ],
-  },
-];
+const categories: { title: string; items: { name: string; src: string }[] }[] =
+  [
+    {
+      title: "Programming Languages",
+      items: [
+        { name: "Java", src: "./images/java.png" },
+        { name: "Python", src: "./images/python.png" },
+        { name: "SQL", src: "https://cdn.simpleicons.org/sqlite" },
+      ],
+    },
+    {
+      title: "Frameworks & Libraries",
+      items: [
+        { name: "Spring", src: "https://cdn.simpleicons.org/spring" },
+        { name: "Spring Boot", src: "https://cdn.simpleicons.org/springboot" },
+        { name: "Hibernate", src: "https://cdn.simpleicons.org/hibernate" },
+        { name: "JUnit 5", src: "https://cdn.simpleicons.org/junit5" },
+      ],
+    },
+    {
+      title: "Tools & Platforms",
+      items: [
+        { name: "Docker", src: "https://cdn.simpleicons.org/docker" },
+        { name: "Git", src: "https://cdn.simpleicons.org/git" },
+        { name: "Maven", src: "https://cdn.simpleicons.org/apachemaven" },
+        { name: "RabbitMQ", src: "https://cdn.simpleicons.org/rabbitmq" },
+        { name: "Twilio", src: "https://cdn.simpleicons.org/twilio" },
+        {
+          name: "Azure DevOps",
+          src: "./images/azure.png",
+        },
+        {
+          name: "IntelliJ IDEA",
+          src: "./images/intellij.png",
+        },
+        {
+          name: "Pycharm",
+          src: "./images/pycharm.png",
+        },
+        {
+          name: "VS Code",
+          src: "./images/vs.png",
+        },
+      ],
+    },
+    {
+      title: "Databases",
+      items: [
+        { name: "MySQL", src: "https://cdn.simpleicons.org/mysql" },
+        { name: "PostgreSQL", src: "https://cdn.simpleicons.org/postgresql" },
+        {
+          name: "SQL Server",
+          src: "./images/sql-server.png",
+        },
+      ],
+    },
+    {
+      title: "APIs & Testing",
+      items: [
+        { name: "Swagger", src: "https://cdn.simpleicons.org/swagger" },
+        { name: "Postman", src: "https://cdn.simpleicons.org/postman" },
+      ],
+    },
+    {
+      title: "Security",
+      items: [
+        { name: "Spring Security", src: "./images/security.png" },
+        { name: "JWT", src: "./images/jwt.png" },
+      ],
+    },
+  ];
 
 export default function TechLogos() {
   return (
